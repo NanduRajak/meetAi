408dc65e70a9e99d611fb80205ee95d8801e712d
Nandu Rajak <golanukondanandu@gmail.com>
2025-06-10 21:07:52 +0530

    08:trcp setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,14 +2,19 @@ import { auth } from "@/lib/auth";
 import { HomeView } from "@/modules/auth/ui/views/home/ui/views/home-view";
 import { redirect } from "next/navigation";
 import { headers } from "next/headers";
+import { caller } from "@/trpc/server";
 
 const Page = async () => {
+  const data = await caller.hello({ text: "Zoro" });
+
   const session = await auth.api.getSession({
     headers: await headers(),
   });
   if (!session) {
     redirect("/sign-in");
   }
+
+  return <p>{data.greeting}</p>;
 
   return <HomeView />;
 };
